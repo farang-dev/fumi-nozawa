89a3585d8465367558434505c61193c2f5a7067b
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2024-11-04 00:18:42 +0400

    Initial commit for GitHub Pages site

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? '/fumi-nozawa' : '', // replace "my-portfolio" with your repo name
 };
 
-export default nextConfig;
+module.exports = nextConfig;
