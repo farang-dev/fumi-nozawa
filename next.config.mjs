70d5ee5518fecf0adacf3b00c23a1f9aa4959d8e
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2024-11-04 00:47:01 +0400

    Deploy Next.js site to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Enables static export
-  images: {
-    unoptimized: true, // Disable image optimization
-  },
 };
 
 export default nextConfig;
