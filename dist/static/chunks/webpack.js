5be242aef034b668dae7570acbce519830de20c7
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2024-11-07 03:17:04 +0400

    asset added

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a43e9f902328430e"; }
+/******/ 		__webpack_require__.h = function() { return "965028eb76c75a74"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
